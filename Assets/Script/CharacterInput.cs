5626b0086d363903a6c333b20ede627c68d7afc1
Raka Aufar Luthfan <45837542+rakaaufardev@users.noreply.github.com>
2023-03-05 17:02:59 +0700

    updated CharacterInput

--- Assets/Script/CharacterInput.cs
+++ Assets/Script/CharacterInput.cs
@@ -1,36 +1,50 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+[RequireComponent(typeof(CharacterAnimator))]
 public class CharacterInput : MonoBehaviour
 {
+    
     [SerializeField] private InputActionReference verticalInput;
     [SerializeField] private InputActionReference horizontalInput;
+    [SerializeField] private InputActionReference sprintInput;
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotateSpeed = 50f;
+    private CharacterAnimator characterAnimator;
 
     private Rigidbody rb;
     private Vector3 moveDirection = Vector3.zero;
 
+    private float speed;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        characterAnimator = GetComponent<CharacterAnimator>();
     }
 
     private void FixedUpdate()
     {
-        // Mendapatkan input untuk gerakan horizontal dan vertikal
-        float moveHorizontal = horizontalInput.action.ReadValue<float>();
-        float moveVertical = verticalInput.action.ReadValue<float>();
+        if (horizontalInput.action.IsPressed() || verticalInput.action.IsPressed())
+        {
+            float moveHorizontal = horizontalInput.action.ReadValue<float>();
+            float moveVertical = verticalInput.action.ReadValue<float>();
 
-        // Mendapatkan arah gerakan karakter
-        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical).normalized;
+            speed = 0.5f;
 
-        // Menggerakkan Rigidbody
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+            Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical).normalized;
 
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
-        Quaternion targetRotation = Quaternion.LookRotation(movement);
-        rb.rotation = Quaternion.RotateTowards(rb.rotation, targetRotation, rotateSpeed * Time.fixedDeltaTime);
+            if (movement != Vector3.zero)
+            {
+                transform.LookAt(transform.position + movement);
+            }
+            if (sprintInput.action.IsPressed())
+            {
+                speed= 1.0f;
+            }
+        }
+        characterAnimator.SetAnimatorSpeed(speed);
     }
 }
